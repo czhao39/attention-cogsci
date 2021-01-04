842a05edbca625fac4a6fc4072fc4f612cc6693c
Haicheng Charles Zhao <czhao39@gmail.com>
2021-01-04 17:27:41 -0500

    Get LTPA internal representations

--- learn-to-pay-attention/get_confidences_and_representations.py
+++ learn-to-pay-attention/get_confidences_and_representations.py
@@ -15,7 +15,8 @@ device = torch.device("cuda" if use_cuda else "cpu")
 
 parser = argparse.ArgumentParser(description="LearnToPayAttn-CIFAR100")
 
-parser.add_argument("-o", "--output", type=str, default="confidences.txt", help="path to output file")
+parser.add_argument("-o1", "--confidences_out", type=str, default="confidences.txt", help="path to output file")
+parser.add_argument("-o2", "--representations_out", type=str, default="representations.npy", help="path to output file")
 parser.add_argument("--image_dir", "-i", required=True, help="path to images")
 parser.add_argument("--model", "-m", required=True, help="path to model")
 parser.add_argument("--attn_mode", type=str, default="before", help="insert attention modules before OR after maxpooling layers")
@@ -75,6 +76,19 @@ def main():
     else:
         min_up_factor = 2
 
+    # Add hook to get internal representation
+    global internal_repr
+    def update_internal_repr(module, inpt, output):
+        global internal_repr
+        #internal_repr = output.flatten()
+        internal_repr = inpt[0].flatten()
+        print(internal_repr[100])
+        print(inpt[0].flatten()[10])
+    model.classify.register_forward_hook(update_internal_repr)
+    #model.dense.register_forward_hook(update_internal_repr)
+    #model.conv_block6.register_forward_hook(update_internal_repr)
+    print(model)
+
     # Load CIFAR-100 classes
     with open(opt.cifar100_classes, "r") as infile:
         cifar_classes = infile.read().split()
@@ -90,11 +104,18 @@ def main():
             top5 = [(cifar_classes[ind], output[ind]) for ind in top5_inds]
             top5.sort(key=lambda x: x[1], reverse=True)
 
-            results.append((dataset.imgs[batch_idx][0], *(x for tup in top5 for x in tup)))
+            rep = internal_repr.cpu().numpy()
+
+            results.append((dataset.imgs[batch_idx][0], *(x for tup in top5 for x in tup), rep))
 
     results.sort()
-    with open(opt.output, "w") as outfile:
-        outfile.write("\n".join("\t".join(map(str, res)) for res in results))
+    with open(opt.confidences_out, "w") as outfile:
+        outfile.write("\n".join("\t".join(map(str, res[:-1])) for res in results))
+    print("Wrote confidences to", opt.confidences_out)
+
+    reprs = np.array([res[-1] for res in results])
+    np.save(opt.representations_out, reprs)
+    print("Wrote internal representations to", opt.representations_out)
 
 
 if __name__ == "__main__":
