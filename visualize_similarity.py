6db31beece19bbc5034f41d414d0631c1c23ea2a
Haicheng Charles Zhao <czhao39@gmail.com>
2021-01-04 17:25:23 -0500

    Visualize correlation and t-SNE instead of covariance and PCA

--- visualize_similarity.py
+++ visualize_similarity.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
 
 # Parse arguments
@@ -38,28 +39,27 @@ def main():
     assert(rep.shape[0] == len(paths))
     print("Loaded representations")
 
-    # Compute and display covariance matrix
-    cov = np.cov(rep)
-    cov_df = pd.DataFrame(cov, index=paths, columns=paths)
+    # Compute and display correlation matrix
+    corr = np.corrcoef(rep)
+    corr_df = pd.DataFrame(corr, index=paths, columns=paths)
     sns.set(style="whitegrid", font_scale=0.2, rc={"xtick.major.pad": -5, "ytick.major.pad": -5})
     plt.figure(figsize=(8, 8))
-    sns.heatmap(cov_df, square=True, cbar=False)
+    sns.heatmap(corr_df, square=True, cbar=False)
     plt.tight_layout()
-    plt.savefig("covariance.png", dpi=400, bbox_inches="tight")
-    plt.show()
+    plt.savefig("correlation.png", dpi=400, bbox_inches="tight")
+    plt.show(block=False)
 
-    # Compute and display projections onto first two principal components
-    pca = PCA(n_components=2)
-    projected = pca.fit_transform(rep)
+    # Compute and display 2D embedding
+    projected = PCA(n_components=50).fit_transform(rep)
+    projected = TSNE(n_components=2, init="pca", learning_rate=20, verbose=2).fit_transform(projected)
     sns.set(style="whitegrid", font_scale=0.7)
     plt.figure(figsize=(8, 6))
     sns.scatterplot(x=projected[:, 0], y=projected[:, 1], hue=classes)
     for i, label in enumerate(paths):
         plt.annotate(label, projected[i], fontsize=4)
     plt.tight_layout()
-    plt.savefig("pca.png", dpi=300, bbox_inches="tight")
+    plt.savefig("embedding.png", dpi=300, bbox_inches="tight")
     plt.show()
-
 
 
 if __name__ == "__main__":
